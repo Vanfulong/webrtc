58a0484951943fd1729abf32bd2b62841a2f0e94
root <root@debian.vanlong>
2023-08-31 12:05:15 -0400

    first commit

---
+++ client/config.js
@@ -0,0 +1,12 @@
+var iceServers = [
+	{
+		urls       : 'turn:voip3.operrtel.com:443?transport=tcp',
+		username   : 'websip',
+		credential : 'websip'
+	},
+	{
+		urls       : 'turns:voip3.operrtel.com:80?transport=tcp',
+		username   : 'websip',
+		credential : 'websip'
+	}
+];
